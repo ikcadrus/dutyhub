74e80720f62fa09ed7811ea960c5c1b67c3e7ed9
ikcadrus <jakub0901.js@gmail.com>
2023-12-04 02:36:31 +0100

    Navbar prototype

--- index.php
+++ index.php
@@ -3,8 +3,16 @@
     <head>
         <title>Home</title>
         <meta charset="UTF-8">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     </head>
     <body>
-        <img src = "img/logo/logo-light.svg">
+        <nav class = "navbar navbar-light bg-light">
+            <a class = "navbar-brand" href="#">
+                <img src = "img/logo/logo-light.svg" width="30" height="30">
+                    DutyHub
+            </a>
+        </nav>
     </body>
 </html>
