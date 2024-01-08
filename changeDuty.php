8c6834110e944709d770639cf0294ab336f576e2
ikcadrus <jakub0901.js@gmail.com>
2024-01-08 01:57:29 +0100

    Project enrichment, new ideas and features

--- changeDuty.php
+++ changeDuty.php
@@ -89,40 +89,22 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="new-task mt-5">
-                    <h2>Add new task</h2>  
+                    <h2>Edit Duty</h2>  
                     <div class="col-8 offset-2 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-4 col-xl-8">
                         <div class="task-section form-floating col-xl-6">
                             <input type="text" name="taskHolder" class="form-control" id="floating-task" placeholder="text" maxlength="50" required>
                             <span class="pull-right label label-default" id="count-newtask"></span>
-                            <label for="floating-task">Insert your task</label>
+                            <label for="floating-task">Change your task</label>
                         </div>
-                        <button type="button" class="btn btn-dark mt-2 col-4 offset-4 col-sm-4 offset-sm-4 col-md-4 offset-md-4 col-lg-4 offset-lg-4 col-xl-2 offset-xl-2">Add Task</button>
-                    </div>
-                </div>
-                <div class="duty-main-text mt-5">
-                    <h2>All your duties</h2>
-                </div>
-                <ul class="duties-list mt-2 col-10 offset-1 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-6 offset-xl-3">
-                    <li class="duty mb-3 row">
-                        <div class="accept form-check col-2 col-sm-2 col-md-1 col-lg-1 col-xl-1">
-                            <input class="form-check-input" type="checkbox" id="checkbox" onclick="changeText()">
-                            <label class="form-check-label" for="checkbox"></label>
-                        </div>
-                        <div class="duty-text col-8 col-sm-8 col-md-9 col-lg-9 col-xl-9 text-break" id="duty-ready">
-                            12345678901234567890123456789012345678901234567890
-                        </div>
-                        <div class="edit col-1">
-                            <a class="duty-icon" href="changeDuty.php">
-                                <i class="bi bi-pencil-square"></i>
-                            </a>    
-                        </div>
-                        <div class="delete col-1">
-                            <a class="duty-icon" href="">
-                                <i class="bi bi-trash"></i>
+                        <button type="button" class="btn btn-dark mt-2 col-4 offset-4 col-sm-4 offset-sm-4 col-md-4 offset-md-4 col-lg-4 offset-lg-4 col-xl-2 offset-xl-2">Save</button>
+                        <div class="back-duties mt-2 col-4 offset-4 col-sm-4 offset-sm-4 col-md-4 offset-md-4 col-lg-4 offset-lg-4 col-xl-2 offset-xl-2 text-center">
+                            <a href="duties.php" class="back-duties-text me-2">
+                            <i class="bi bi-chevron-left"></i>
+                            Back 
                             </a>
                         </div>
-                    </li>
-                </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </body>
