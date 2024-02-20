12e6b108202d5184fc748ce590a6e9c8734df35a
ikcadrus <jakub0901.js@gmail.com>
2024-02-20 02:20:45 +0100

    Error 404

---
+++ error404.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Error 404</title>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+        <link rel="stylesheet" href="styles/error404.css?v=<?php echo time(); ?>">
+    </head>
+    <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="left col-12 col-md-6 bg-light">
+                <div class="">
+                    <a class="logo">
+                        <img src="img/logo/logo-light.svg" class="img-fluid" alt="Logo">
+                    </a>
+                </div>
+            </div>
+            <div class="right col-12 col-md-6 mt-5 mb-5 d-flex justify-content-center align-items-center">
+            <div class="row">
+                <div class="not-found">
+                    <h1>PAGE NOT FOUND</h1>
+                </div>
+                <div class="text d-flex justify-content-center align-items-center ">
+                    <h1>ERROR</h1>
+                </div>
+                <div class="error">
+                    <div>404</div>
+                </div>
+                <div class="back-option">
+                    <h1>Want to return to the homepage?</h1>
+                    <a href="index.php"><button type="button" class="btn btn-dark col-4 offset-4 col-sm-2 offset-sm-5 col-md-4 offset-md-4 col-lg-4 offset-lg-4 col-xl-2 offset-xl-5">Click me</button></a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
